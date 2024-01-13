a7f05fd4a5b58d4897be80bd9f4cb645b34c43d1
Adam Marshall <adam@appsynergy.net>
2024-01-13 11:07:11 +0000

    Seems to generally work

--- numeralToNumber.test.ts
+++ numeralToNumber.test.ts
@@ -10,11 +10,6 @@ test("Should identify non valid numbers", () => {
   expect(numeralToNumber("I'm not a number")).toBe(NaN);
 });
 
-test("Note that behaviour is misleading for negative numbers", () => {
-  expect(numeralToNumber("minus Six")).toBe(6);
-});
-
-
 test("Should identify 1 digit numbers", () => {
   expect(numeralToNumber("zero")).toBe(0);
   expect(numeralToNumber("one")).toBe(1);
@@ -30,6 +25,40 @@ test("Should identify 2 digit numbers", () => {
 });
 
 test("Should identify 3 digit numbers", () => {
+  expect(numeralToNumber("two hundred")).toBe(200);
   expect(numeralToNumber("Eight hundred and twelve")).toBe(812);
   expect(numeralToNumber("nine-hundred and ninety-nine")).toBe(999);
+});
+
+test("Should identify 4 digit numbers", () => {
+  expect(numeralToNumber("twelve hundred")).toBe(1200);
+  expect(numeralToNumber("one thousand and six")).toBe(1006);
+  expect(numeralToNumber("four thousand and twenty-five")).toBe(4025);
+  expect(numeralToNumber("nine thousand, nine hundred and sixty")).toBe(9960);
+});
+
+test("Should identify 5 digit numbers", () => {
+  expect(numeralToNumber("forty thousand")).toBe(40000);
+  expect(numeralToNumber("sixty thousand, two hundred and forty three")).toBe(60243);
+});
+
+test("Should identify 6 digit numbers", () => {
+  expect(numeralToNumber("three hundred thousand")).toBe(300000);
+  expect(numeralToNumber("three hundred thousand and one")).toBe(300001);
+  expect(numeralToNumber("three hundred thousand, nine hundred and one")).toBe(300901);
+  expect(numeralToNumber("five hundred four thousand and sixty two")).toBe(504062);
+  expect(numeralToNumber("Seven hundred and forty nine thousand, five hundred and eighty one")).toBe(749581);
+});
+
+test("Should identify simple millions", () => {
+  expect(numeralToNumber("one million")).toBe(1000000);
+  expect(numeralToNumber("twelve million")).toBe(12000000);
+});
+
+test("Note more complex use of millions does not give the correct answer", () => {
+  expect(numeralToNumber("twelve million, twenty four thousand")).not.toBe(12024000);
+});
+
+test("Note that negative numbers do not give the correct answer", () => {
+  expect(numeralToNumber("minus Six")).toBe(6);
 });
--- numeralToNumber.ts
+++ numeralToNumber.ts
@@ -1,8 +1,10 @@
 export const numeralToNumber = (str: string): number => {
+  // convert any string to a number from 0 to 1 million
+  // note that numbers outside this range will not work correctly - maybe should return NaN for these too?
+  // if the string cannot be converted to a number then NaN is returned
 
   const words: string[] = str.split(/[\s,-]+/);
-
-  // convert any string to a number from 0 to 1 million
+  
   const earlyNumbers = ["zero", "one", "two", "three",
     "four", "five", "six", "seven",
     "eight", "nine", "ten", "eleven",
@@ -12,22 +14,35 @@ export const numeralToNumber = (str: string): number => {
   const tensNumbers = ["twenty", "thirty", "forty",
     "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-  const digitNumbers = ["one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine"]
-
   let answer = 0;
+  let lastFoundNumber = 0;
   let foundAnyNumbers = false;
 
   words.map((word) => {
-    const foundSmallNumber = earlyNumbers.indexOf(word.toLowerCase());
+    const lowerWord = word.toLowerCase();
+    if (lowerWord == "million") {
+      answer += answer * 999999;
+    }
+
+    if (lowerWord == "thousand") {
+      answer += answer * 999;
+    }
+
+    if (lowerWord == "hundred") {
+      answer += lastFoundNumber * 99;
+    }
+
+    const foundSmallNumber = earlyNumbers.indexOf(lowerWord);
     if (foundSmallNumber >= 0) {
       foundAnyNumbers = true;
+      lastFoundNumber = foundSmallNumber;
       answer += foundSmallNumber;
     }
 
-    const foundTensNumber = tensNumbers.indexOf(word.toLowerCase());
+    const foundTensNumber = tensNumbers.indexOf(lowerWord);
     if (foundTensNumber >= 0) {
       foundAnyNumbers = true;
+      lastFoundNumber = (foundTensNumber + 2) * 10;
       answer += (foundTensNumber + 2) * 10;
     }
   })
