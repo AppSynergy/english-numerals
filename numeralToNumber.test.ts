5cae5b58197eb1b072b54eb5bbdec249182207a5
Adam Marshall <adam@appsynergy.net>
2024-01-13 10:14:56 +0000

    one and two digit numbers

--- numeralToNumber.test.ts
+++ numeralToNumber.test.ts
@@ -5,3 +5,31 @@ test("Should return a number", () => {
 
   expect(typeof result).toBe("number");
 });
+
+test("Should identify non valid numbers", () => {
+  expect(numeralToNumber("I'm not a number")).toBe(NaN);
+});
+
+test("Note that behaviour is misleading for negative numbers", () => {
+  expect(numeralToNumber("minus Six")).toBe(6);
+});
+
+
+test("Should identify 1 digit numbers", () => {
+  expect(numeralToNumber("zero")).toBe(0);
+  expect(numeralToNumber("one")).toBe(1);
+  expect(numeralToNumber("three")).toBe(3);
+  expect(numeralToNumber("seven")).toBe(7);
+});
+
+test("Should identify 2 digit numbers", () => {
+  expect(numeralToNumber("Twenty-four")).toBe(24);
+  expect(numeralToNumber("thirty seven")).toBe(37);
+  expect(numeralToNumber("eighty eight")).toBe(88);
+  expect(numeralToNumber("ninety-one")).toBe(91);
+});
+
+test("Should identify 3 digit numbers", () => {
+  expect(numeralToNumber("Eight hundred and twelve")).toBe(812);
+  expect(numeralToNumber("nine-hundred and ninety-nine")).toBe(999);
+});
--- numeralToNumber.ts
+++ numeralToNumber.ts
@@ -1,3 +1,40 @@
 export const numeralToNumber = (str: string): number => {
-  return NaN;
+
+  const words: string[] = str.split(/[\s,-]+/);
+
+  // convert any string to a number from 0 to 1 million
+  const earlyNumbers = ["zero", "one", "two", "three",
+    "four", "five", "six", "seven",
+    "eight", "nine", "ten", "eleven",
+    "twelve", "thirteen", "fourteen", "fifteen",
+    "sixteen", "seventeen", "eighteen", "nineteen"]
+
+  const tensNumbers = ["twenty", "thirty", "forty",
+    "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  const digitNumbers = ["one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"]
+
+  let answer = 0;
+  let foundAnyNumbers = false;
+
+  words.map((word) => {
+    const foundSmallNumber = earlyNumbers.indexOf(word.toLowerCase());
+    if (foundSmallNumber >= 0) {
+      foundAnyNumbers = true;
+      answer += foundSmallNumber;
+    }
+
+    const foundTensNumber = tensNumbers.indexOf(word.toLowerCase());
+    if (foundTensNumber >= 0) {
+      foundAnyNumbers = true;
+      answer += (foundTensNumber + 2) * 10;
+    }
+  })
+
+  return foundAnyNumbers ? answer : NaN;
 };
+
+
+
+
